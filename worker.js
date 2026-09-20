e7712dd77233a648fcc3489e479c21c52f34f507
airlock11 <121549829+airlock11@users.noreply.github.com>
2026-09-21 03:43:28 +0800

    Add WNBA Cloudflare score fallback

--- worker.js
+++ worker.js
@@ -17,6 +17,7 @@ export default {
       const paths = {
         soccer: "soccer/eng.1",
         basketball: "basketball/nba",
+        wnba: "basketball/wnba",
         baseball: "baseball/mlb",
         hockey: "hockey/nhl",
         football: "football/nfl",
