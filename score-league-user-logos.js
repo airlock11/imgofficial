3aa525e5636d27488bf0e20339e88102340a6c68
airlock11 <121549829+airlock11@users.noreply.github.com>
2026-09-21 15:02:51 +0800

    Use uploaded boxing gloves logo

--- score-league-user-logos.js
+++ score-league-user-logos.js
@@ -14,5 +14,5 @@ volleyball_m:"data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAQDAwMDAgQ
 ,
 fivb:"https://www.fivb.org/Vis2009/Images/GetImage.asmx?No=202231632&maxSize=1400"
 ,
-boxing:"assets/leagues/boxing-glove.svg?v=20260921-2"
+boxing:"assets/leagues/boxing-user-logo.jpg?v=20260921-1"
 };
