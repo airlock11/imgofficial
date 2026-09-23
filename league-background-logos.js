2a2fab8453e4ca890f7ada7b421ebe30bf5458ff
airlock11 <121549829+airlock11@users.noreply.github.com>
2026-09-23 13:48:17 +0800

    Add dedicated high-quality league hero background images

---
+++ league-background-logos.js
@@ -0,0 +1,31 @@
+// Dedicated high-resolution hero/background artwork.
+// This map is intentionally separate from score/list logos so league pages can use
+// a cleaner, larger source without changing the normal logo shown elsewhere.
+window.IMG_LEAGUE_BACKGROUND_LOGOS={
+  asian_games:"https://olympicthai.org/wp-content/uploads/2024/05/LOGO-4.png",
+  nba:"https://cdn.nba.com/logos/leagues/logo-nba.svg",
+  wnba:"https://cdn.wnba.com/logos/leagues/logo-wnba.svg",
+  pba:"https://www.google.com/s2/favicons?domain=pba.ph&sz=512",
+  mpbl:"https://www.google.com/s2/favicons?domain=mpbl.com.ph&sz=512",
+  "nbl-pilipinas":"https://www.google.com/s2/favicons?domain=nblpilipinas.com&sz=512",
+  "nbl-australia":"https://www.google.com/s2/favicons?domain=nbl.com.au&sz=512",
+  vba:"https://www.google.com/s2/favicons?domain=vba.vn&sz=512",
+  "b-league":"https://commons.wikimedia.org/wiki/Special:Redirect/file/B.League_logo.svg",
+  euroleague:"https://commons.wikimedia.org/wiki/Special:Redirect/file/EuroLeague_logo.svg",
+  uaap:"https://www.google.com/s2/favicons?domain=uaap.org&sz=512",
+  "ncaa-philippines":"https://www.google.com/s2/favicons?domain=ncaa.org.ph&sz=512",
+  "premier-league":"https://logo.premierleague.com/img/logo-negative_1.svg",
+  "la-liga":"https://commons.wikimedia.org/wiki/Special:Redirect/file/LaLiga_logo_2023.svg",
+  "serie-a":"https://commons.wikimedia.org/wiki/Special:Redirect/file/Serie_A_logo_2022.svg",
+  bundesliga:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Bundesliga_logo_(2017).svg",
+  "champions-league":"https://commons.wikimedia.org/wiki/Special:Redirect/file/UEFA_Champions_League_logo_2.svg",
+  mls:"https://commons.wikimedia.org/wiki/Special:Redirect/file/MLS_crest_logo_RGB_gradient.svg",
+  atp:"https://commons.wikimedia.org/wiki/Special:Redirect/file/ATP_Tour_logo.svg",
+  wta:"https://commons.wikimedia.org/wiki/Special:Redirect/file/WTA_logo_2020.svg",
+  ipl:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Indian_Premier_League_Official_Logo.svg",
+  mlb:"https://commons.wikimedia.org/wiki/Special:Redirect/file/Major_League_Baseball_logo.svg",
+  nhl:"https://commons.wikimedia.org/wiki/Special:Redirect/file/National_Hockey_League_shield.svg",
+  nfl:"https://commons.wikimedia.org/wiki/Special:Redirect/file/National_Football_League_logo.svg",
+  "formula-1":"https://commons.wikimedia.org/wiki/Special:Redirect/file/F1.svg",
+  ufc:"https://commons.wikimedia.org/wiki/Special:Redirect/file/UFC_Logo.svg"
+};
