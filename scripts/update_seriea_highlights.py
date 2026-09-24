9320e7714ee277f779d316c66cacd3639315bf2c
airlock11 <121549829+airlock11@users.noreply.github.com>
2026-09-25 01:05:36 +0800

    Add independent football highlight updater

---
+++ scripts/update_seriea_highlights.py
@@ -0,0 +1,2 @@
+from football_highlights_common import run
+run({"leagueKey":"seriea","league":"Serie A","output":"seriea-highlights.json","mode":"youtube","handle":"seriea","includeAny":["HIGHLIGHT"],"sourceName":"Serie A Official YouTube","sourceUrl":"https://www.youtube.com/@seriea","limit":12})
