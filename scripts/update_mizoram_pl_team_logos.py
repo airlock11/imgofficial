faaca927e17b5717b9eeb3c1fd8aefe89523f839
airlock11 <121549829+airlock11@users.noreply.github.com>
2026-09-25 01:41:22 +0800

    Add independent football team logo updater

---
+++ scripts/update_mizoram_pl_team_logos.py
@@ -0,0 +1,11 @@
+from football_team_logos_common import run
+run({"leagueKey":"mizoram_pl","league":"Mizoram Premier League","output":"mizoram-pl-teams.json","sourcePage":"https://footballin.in/mizoram-fa/","teams":[
+{"name":"Aizawl FC","lookup":"Aizawl FC"},
+{"name":"Mizoram Police FC","lookup":"Mizoram Police FC"},
+{"name":"Chanmari FC","lookup":"Chanmari FC"},
+{"name":"MLS FC","lookup":"MLS FC","aliases":["Mls FC Lawtngtlai"]},
+{"name":"Dinthar FC","lookup":"Dinthar FC"},
+{"name":"Ramthar Veng FC","lookup":"Ramthar Veng FC"},
+{"name":"Saikhamakawn FC","lookup":"Saikhamakawn FC"},
+{"name":"Kanan FC","lookup":"Kanan FC","aliases":["Kanan FC Aizawl"]}
+]})
