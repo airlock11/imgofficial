483344768d848b660d61791f8cbc9da8f9a97506
airlock11 <121549829+airlock11@users.noreply.github.com>
2026-09-25 01:06:01 +0800

    Add independent football highlight updater

---
+++ scripts/update_pfl_highlights.py
@@ -0,0 +1,2 @@
+from football_highlights_common import run
+run({"leagueKey":"pfl","league":"PFL","output":"pfl-highlights.json","mode":"youtube","channelId":"UCewlC5rcsENhcjnrsfvIpaw","includeAny":["HIGHLIGHT"],"sourceName":"Philippines Football League Official YouTube","sourceUrl":"https://www.youtube.com/@WeArePFL","limit":12})
