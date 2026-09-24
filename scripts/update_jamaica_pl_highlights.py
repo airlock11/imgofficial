cc6040eb7c37bdadd774e3a58d541c9f550732ec
airlock11 <121549829+airlock11@users.noreply.github.com>
2026-09-25 01:06:04 +0800

    Add independent football highlight updater

---
+++ scripts/update_jamaica_pl_highlights.py
@@ -0,0 +1,2 @@
+from football_highlights_common import run
+run({"leagueKey":"jamaica_pl","league":"Jamaican Premier League","output":"jamaica-pl-highlights.json","mode":"youtube","seedVideo":"q2IVYm24JOY","includeAny":["HIGHLIGHT"],"sourceName":"Jamaica Premier League TV","sourceUrl":"https://www.youtube.com/","limit":12})
