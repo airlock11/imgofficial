b2acaf6a9c078e4332393972086dc3fc8dfee1bc
airlock11 <121549829+airlock11@users.noreply.github.com>
2026-09-25 01:41:24 +0800

    Add independent football team logo updater

---
+++ scripts/update_el_salvador_reserves_team_logos.py
@@ -0,0 +1,15 @@
+from football_team_logos_common import run
+run({"leagueKey":"el_salvador_reserves","league":"Primera Division, Reserves","output":"el-salvador-reserves-teams.json","sourcePage":"https://globalsportsarchive.com/en/soccer/competition/liga-pepsi-primera-division-2026-2027/80363","teams":[
+{"name":"Alianza FC U20","lookup":"Alianza FC","aliases":["Alianza FC","Alianza San Salvador II"]},
+{"name":"CD FAS U20","lookup":"CD FAS","aliases":["CD FAS II U20"]},
+{"name":"Inter Formando Un Atleta U20","lookup":"Inter FA","aliases":["Inter FA Reserves"]},
+{"name":"C.D. Platense U20","lookup":"CD Platense","aliases":["CD Platense Zacatecoluca II U20"]},
+{"name":"AD Isidro Metapan U20","lookup":"AD Isidro Metapán","aliases":["Isidro II","AD Isidro Metapan"]},
+{"name":"Águila U20","lookup":"CD Águila","aliases":["Aguila II","CD Aguila U20"]},
+{"name":"CD Inca Aruba U20","lookup":"CD Inca Aruba","aliases":["Inca Aruba U20"]},
+{"name":"CD Fuerte San Francisco U20","lookup":"CD Fuerte San Francisco","aliases":["CD Fuerte San Francisco II"]},
+{"name":"L.Á. Firpo U20","lookup":"CD Luís Ángel Firpo","aliases":["C.D. Luis Ángel Firpo II","Luis Angel Firpo U20"]},
+{"name":"Balboa U20","lookup":"CD Atlético Balboa","aliases":["CD Atletico Balboa U20"]},
+{"name":"CD Cacahuatique U20","lookup":"CD Cacahuatique","aliases":["Cacahuatique II"]},
+{"name":"Municipal Limeño U20","lookup":"CD Municipal Limeño","aliases":["C.D. Municipal Limeno II U20","Municipal Limeno U20"]}
+]})
