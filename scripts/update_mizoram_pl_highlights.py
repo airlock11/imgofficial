c43c0f8fda8463b6486d14ae21925b8d3bb71e06
airlock11 <121549829+airlock11@users.noreply.github.com>
2026-09-25 01:06:06 +0800

    Add independent football highlight updater

---
+++ scripts/update_mizoram_pl_highlights.py
@@ -0,0 +1,2 @@
+from football_highlights_common import run
+run({"leagueKey":"mizoram_pl","league":"Mizoram Premier League","output":"mizoram-pl-highlights.json","mode":"youtube","seedVideo":"AGkhT5sxmSo","includeAny":["MPL"],"requireAny":["HIGHLIGHT"],"sourceName":"Zonet","sourceUrl":"https://www.youtube.com/","limit":12})
